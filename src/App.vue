7ca5d16f4502dc9882219a8d0baeedea45e1e77f
Darius Baciu <baciu.darius01@gmail.com>
2023-11-24 21:30:09 +0200

    modal css changes

--- src/App.vue
+++ src/App.vue
@@ -35,10 +35,10 @@
      <div v-if="showModal" class="overlay">
       <div class="modal">
         {{ newNote  }}
+        <button class="close" @click="showModal = false">&times;</button>
         <textarea v-model.trim="newNote" name="note" id="note" cols="30" rows="10"></textarea>
         <p v-if="errorMessage">{{ errorMessage }}</p>
         <button @click="addNote">Add Note</button>
-        <button class="close" @click="showModal = false">Close</button>
       </div>
     </div> 
     <div class="container">
@@ -133,28 +133,46 @@
 
   .modal {
     width: 750px;
-    background-color: white;
+    background: rgb(61,181,251);
+    background: -moz-linear-gradient(150deg, rgba(61,181,251,1) 10%, rgba(178,125,202,1) 59%);
+    background: -webkit-linear-gradient(150deg, rgba(61,181,251,1) 10%, rgba(178,125,202,1) 59%);
+    background: linear-gradient(150deg, rgba(61,181,251,1) 10%, rgba(178,125,202,1) 59%);
+    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#3db5fb",endColorstr="#b27dca",GradientType=1);
     border-radius: 10px;
     padding: 30px;
     position: relative;
     display: flex;
     flex-direction: column;
+    border: none;
   }
+   .modal textarea {
+    border: none;
+    border-radius: 8px;
+   }
 
   .modal button {
     padding: 10px 20px;
     font-size: 20px;
     width: 100%;
-    background-color: blueviolet;
+    background-color: #e238e7;
     border: none;
     color: white;
     cursor: pointer;
-    margin-top: 15px
+    margin-top: 15px;
+    border-radius: 8px;
   }
 
   .modal .close {
-    background-color: rgb(193, 15, 15);
+    background-color: #ccc;
     margin-top: 7px;
+    border-radius: 70%;
+    display: flex;
+    justify-content: center;
+    position: relative;
+    height: 40px;
+    width: 40px;
+    bottom: 33px;
+    left: 668px;
   }
   .modal p {
     color: rgb(193, 15, 15);
